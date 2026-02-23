277a9da54de44cc3a499efd8f18539ae0dce5910
Anilcan Cakir <anilcan.cakir@gmail.com>
2026-02-23 17:07:12 +0300

    feat: add barrel export (magic_starter.dart)

---
+++ lib/magic_starter.dart
@@ -0,0 +1,29 @@
+// Magic Starter plugin exports
+
+export 'src/facades/magic_starter.dart';
+export 'src/magic_starter_manager.dart'
+    show MagicStarterManager, UserModelFactory, TeamResolverConfig;
+export 'src/providers/magic_starter_service_provider.dart';
+export 'src/ui/view_registry.dart';
+export 'src/configuration/magic_starter_config.dart';
+export 'src/models/starter_team.dart';
+export 'src/http/controllers/auth_controller.dart';
+export 'src/models/magic_starter_auth_user.dart';
+export 'src/http/controllers/profile_controller.dart';
+export 'src/http/controllers/team_controller.dart';
+export 'src/routes/auth_routes.dart';
+export 'src/routes/profile_routes.dart';
+export 'src/routes/team_routes.dart';
+export 'src/ui/views/auth/login_view.dart';
+export 'src/ui/views/auth/register_view.dart';
+export 'src/ui/views/auth/forgot_password_view.dart';
+export 'src/ui/views/auth/reset_password_view.dart';
+export 'src/ui/views/profile/profile_settings_view.dart';
+export 'src/ui/views/teams/team_create_view.dart';
+export 'src/ui/views/teams/team_settings_view.dart';
+export 'src/ui/views/teams/team_invitation_accept_view.dart';
+export 'src/ui/layouts/app_layout.dart';
+export 'src/ui/layouts/guest_layout.dart';
+export 'src/ui/widgets/auth_form_card.dart';
+export 'src/ui/widgets/team_selector.dart';
+export 'src/ui/widgets/starter_page_header.dart';
